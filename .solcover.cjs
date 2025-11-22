1246328a2b264f77040f8e5bdad59f607d055d4d
BillieWintheiser <158210965+BillieWintheiser@users.noreply.github.com>
2025-11-22 18:13:01 +0800

    feat: add new features: .solcover

---
+++ .solcover.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  skipFiles: ['test/', 'mock/'],
+  configureYulOptimizer: true,
+  solcOptimizerDetails: {
+    peephole: false,
+    inliner: false,
+    jumpdestRemover: false,
+    orderLiterals: true,
+    deduplicate: false,
+    cse: false,
+    constantOptimizer: false,
+    yul: true,
+  },
+};
