7e2897d5524ba7677e3ccd753a7102fcbcb81483
BillieWintheiser <158210965+BillieWintheiser@users.noreply.github.com>
2025-11-22 18:13:08 +0800

    feat: add new features: hardhat.config

---
+++ hardhat.config.cjs
@@ -0,0 +1,54 @@
+const { task } = require("hardhat/config");
+require("@fhevm/hardhat-plugin");
+require("@nomicfoundation/hardhat-chai-matchers");
+require("@nomicfoundation/hardhat-ethers");
+require("@nomicfoundation/hardhat-verify");
+require("@typechain/hardhat");
+require("hardhat-deploy");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+
+const INFURA_API_KEY = process.env.INFURA_API_KEY || "";
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "0x0000000000000000000000000000000000000000000000000000000000000001";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: {
+    version: "0.8.24",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 800,
+      },
+      evmVersion: "cancun",
+    },
+  },
+  networks: {
+    hardhat: {
+      chainId: 31337,
+    },
+    sepolia: {
+      chainId: 11155111,
+      url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
+      accounts: [PRIVATE_KEY],
+    },
+  },
+  typechain: {
+    outDir: "types",
+    target: "ethers-v6",
+  },
+  gasReporter: {
+    currency: "USD",
+    enabled: process.env.REPORT_GAS ? true : false,
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
+  },
+  mocha: {
+    timeout: 40000,
+  },
+};
